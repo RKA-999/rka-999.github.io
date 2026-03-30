c5c9b2d0a29e2f6ca2be512004b8ff2f924f4e53
R@KA <rahitkumaradhekary161369@gmail.com>
2026-03-30 19:07:29 +0600

    Update history.js

--- history.js
+++ history.js
@@ -156,3 +156,8 @@ window.addEventListener('load', function() {
     }
 });
 
+// Will not download Any image
+document.querySelectorAll('img').forEach(img => {
+  img.addEventListener('contextmenu', e => e.preventDefault());
+  img.addEventListener('dragstart', e => e.preventDefault());
+});
