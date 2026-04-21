32e3eeaf91fd456b2e8bfc875381bb3767185c7c
R@KA <rahitkumaradhekary161369@gmail.com>
2026-04-21 22:25:40 +0600

    Update sw.js

--- sw.js
+++ sw.js
@@ -24,25 +24,14 @@ self.addEventListener("fetch", e => {
 
 
 // Firebase Background Messaging
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-app-compat.js');
-importScripts('https://www.gstatic.com/firebasejs/9.22.0/firebase-messaging-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
 firebase.initializeApp({
-    apiKey: "AIzaSyCJm0C-1TCoR3B1kjNEf13uvvcO9C1lIU8",
-    authDomain: "bajus-d2fdd.firebaseapp.com",
-    projectId: "bajus-d2fdd",
-    storageBucket: "bajus-d2fdd.firebasestorage.app",
-    messagingSenderId: "300016752497",
-    appId: "1:300016752497:web:e8d409a90a9bebc5df48d0"
+  apiKey: "AlzaSyCJm0C-1TCoR3BtkjNEf13avvcO9C11lU8",
+  projectId: "bajus-d2fdd",
+  messagingSenderId: "300016752497",
+  appId: "1:300016752497:web:e8d419a90a9bebc5df48d0"
 });
 
 const messaging = firebase.messaging();
-
-messaging.onBackgroundMessage((payload) => {
-  const notificationTitle = payload.notification.title;
-  const notificationOptions = {
-    body: payload.notification.body,
-    icon: './favicon.png'
-  };
-
-  self.registration.showNotification(notificationTitle, notificationOptions);
-});
